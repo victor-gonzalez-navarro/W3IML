2a2d2326216b6ab6ee3cb976019972e9fb66a7f1
Alejandro Ariza <alejandro.ariza14@gmail.com>
2018-11-27 15:28:16 +0100

    IB3

--- algorithms/ib3Algorithm.py
+++ algorithms/ib3Algorithm.py
@@ -7,13 +7,13 @@ class ib3Algorithm():
     trn_data = None
     trn_labels = None
     tst_labels = None
+    z = 2.58   # z90 = 1.645
 
     def __init__(self, k=1, metric='euclidean'):
         self.k = k
         if metric == 'euclidean':
             self.d = euclidean
 
-    z90 = 1.645
 
     def get_accepted_ys(self, labels_count, incorrect_labels, accuracies, num_processed):
         accepted_idx = []
@@ -22,8 +22,8 @@ class ib3Algorithm():
             accuracy = accuracies[index]
             label_freq = labels_count[incorrect_labels[index]] / num_processed
 
-            limit_conf_accu = np.sqrt(accuracy * (1 - accuracy) / 100)
-            limit_conf_freq = np.sqrt(label_freq * (1 - label_freq) / 100)
+            limit_conf_accu = np.sqrt(accuracy * (1 - accuracy) / 100) * self.z
+            limit_conf_freq = np.sqrt(label_freq * (1 - label_freq) / 100) * self.z
 
             if (accuracy - limit_conf_accu) > (label_freq + limit_conf_freq):
                 accepted_idx.append(index)
@@ -33,8 +33,8 @@ class ib3Algorithm():
     def is_rejected_y(self, labels_count, incorrect_labels, accuracy, num_processed, index):
 
         label_freq = labels_count[incorrect_labels[index]] / num_processed
-        limit_conf_accu = np.sqrt(accuracy * (1 - accuracy) / 100)
-        limit_conf_freq = np.sqrt(label_freq * (1 - label_freq) / 100)
+        limit_conf_accu = np.sqrt(accuracy * (1 - accuracy) / 100) * self.z
+        limit_conf_freq = np.sqrt(label_freq * (1 - label_freq) / 100) * self.z
 
         if (accuracy + limit_conf_accu) < (label_freq - limit_conf_freq):
             return True
@@ -52,6 +52,7 @@ class ib3Algorithm():
             labels_count[label] = 0
 
         for j in range(1, trn_data.shape[0]):
+
             # Step 1: Obtain the similarities with each sample at the Content Description
             similarities = [-self.d(trn_data[j, :], trn_sample) for trn_sample in trn_data_keep]
 
@@ -62,7 +63,10 @@ class ib3Algorithm():
                                                                            classi_list], j)
 
             if (len(accepted_ys) > 0):
-                neighbor = np.argmax(similarities[accepted_ys])
+                if len(accepted_ys) == 1:
+                    neighbor = accepted_ys[0]
+                else:
+                    neighbor = accepted_ys[np.argmax(similarities[accepted_ys])]
             else:
                 # Step 2.1: randomly select a number in the range between 1 and the length of the Content Description
                 ith = np.random.random_integers(1, len(trn_data_keep))
@@ -71,16 +75,17 @@ class ib3Algorithm():
                 neighbor = np.argsort(similarities)[-ith]
 
             # Step 3: If classification is incorrect, add it to the Content Description
-            # Note: Is there an error
-            if labels[j] != labels_keep[neighbor]:
+            if labels[j] == labels_keep[neighbor]:
                 pass
             else:
                 trn_data_concat = trn_data[j, :].reshape(1, len(trn_data[j, :]))
                 trn_data_keep = np.concatenate((trn_data_keep, trn_data_concat), axis=0)
                 labels_keep = np.concatenate((labels_keep, np.array(labels[j]).reshape(1)))
+                classi_list.append([])
 
             # Step 4: For each sample in the Content Description:
-            for m in range(trn_data_keep.shape[0]):
+            remove_idx = []
+            for m in range(len(similarities)):
                 if similarities[m] >= similarities[neighbor]:
                     # Step 4.1: Update y's classification record
                     if labels[j] == labels_keep[m]:
@@ -90,9 +95,17 @@ class ib3Algorithm():
 
                     # Step 4.2: If record is poor
                     if self.is_rejected_y(labels_count, labels_keep, np.mean(classi_list[m]), j, m):
-                        classi_list.pop(m)
-                        trn_data_keep = np.delete(trn_data_keep, m, axis=0)
-                        labels_keep = np.delete(labels_keep, m, axis=0)
+                        remove_idx.append(m)
+
+            for m in sorted(remove_idx, reverse=True):
+                classi_list.pop(m)
+            trn_data_keep = np.delete(trn_data_keep, remove_idx, axis=0)
+            labels_keep = np.delete(labels_keep, remove_idx, axis=0)
+
+        self.trn_data = trn_data_keep
+        self.trn_labels = labels_keep
+        print(str(len(trn_data_keep)))
+        print(str(len(labels_keep)))
 
 
 
