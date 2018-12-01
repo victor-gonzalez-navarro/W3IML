4510b5fd9c1b8268ae17fba4c13066507f5b36bd
Gonav <gonav@MacBook-de-Victor-Gonzalez.local>
2018-12-01 11:49:39 +0100

    -

--- algorithms/distances.py
+++ algorithms/distances.py
@@ -1,10 +1,19 @@
 import numpy as np
 
-def euclidean(a, b):
-    return np.sum((a-b)**2)
+#def euclidean(a, b):
+#    return np.sum((a-b)**2)
 
-def euclidean2(a, b):
-    return np.sqrt(np.sum((a-b)**2))
+#def euclidean2(a, b):
+#    return np.sqrt(np.sum((a-b)**2))
+
+def euclidean(a, b):
+    distance = 0
+    for ai,bi in zip(a,b):
+        distance = distance + (ai-bi)**2
+    return distance
 
 def manhattan(a, b):
-    return np.sqrt(np.sum(abs(a - b)))
+    distance = 0
+    for ai,bi in zip(a,b):
+        distance = distance + abs(ai-bi)
+    return distance
